74c20f7c80a00d898c8d6f2529f4a43ddd86b414
Abolfazl Mahmoodi <msd.soft@gmail.com>
2014-03-31 18:17:27 +0430

    201403311817

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :body, :picture
   mount_uploader :picture, PictureUploader
+
+  def to_param
+    normalized_title = title.gsub(' ', '-')
+    "#{self.id}-#{normalized_title}"
+  end
 end
