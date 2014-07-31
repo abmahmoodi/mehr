9749b4c1b55b7c0bbc4aac39e67fe907d63796f4
Abolfazl Mahmoodi <msd.soft@gmail.com>
2014-07-31 13:37:26 +0430

    add disqus

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :body, :picture
   mount_uploader :picture, PictureUploader
 
+  DISQUS_SHORTNAME = Rails.env == "development" ? "dev_shortname".freeze : "production_shortname".freeze
+
   def to_param
     normalized_title = title.gsub(' ', '-')
     "#{self.id}-#{normalized_title}"
